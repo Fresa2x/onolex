3ea5e9f3e2cf1e7b084aca2b94361f3dceec8633
Fresa2x <dashaun75@cameraity.com>
2024-10-25 20:04:17 +0200

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
   const url = new URL(request.url)
 
-  const proxyUrl = 'https://chestieludini.pro'
+  const proxyUrl = 'https://kunuhyges.xyz'
   const backendUrl = `${proxyUrl}${url.pathname}`
 
   const headers = new Headers(request.headers)
